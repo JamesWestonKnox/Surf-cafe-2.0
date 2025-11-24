4db6bc1e759b259bbb64f9c6002f77754f8223e7
JaredAllen777 <ST10271869@vcconnect.edu.za>
2025-11-24 14:39:59 +0200

    Added Login Form using toolbox

---
+++ Surf-Cafe/Login.cs
@@ -0,0 +1,20 @@
+namespace Surf_Cafe
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
