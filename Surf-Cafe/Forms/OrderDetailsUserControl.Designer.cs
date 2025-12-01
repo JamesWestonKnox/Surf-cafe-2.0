0d8be345a0a79060fe6fc2a16ae3dd29e37fd11e
James Knox <jamesknox361@gmail.com>
2025-12-01 14:29:22 +0200

    Added buttons to order data grid view for qty and deletion, button functionality must still be added

--- Surf-Cafe/Forms/OrderDetailsUserControl.Designer.cs
+++ Surf-Cafe/Forms/OrderDetailsUserControl.Designer.cs
@@ -29,7 +29,11 @@
         private void InitializeComponent()
         {
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
             btnSave = new Button();
             btnPay = new Button();
             btnLoyalty = new Button();
@@ -38,6 +42,9 @@
             Product = new DataGridViewTextBoxColumn();
             Quantity = new DataGridViewTextBoxColumn();
             Price = new DataGridViewTextBoxColumn();
+            IncreaseQty = new DataGridViewButtonColumn();
+            DecreaseQty = new DataGridViewButtonColumn();
+            Delete = new DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
@@ -89,7 +96,7 @@
             // 
             // splitContainer1.Panel2
             // 
-            splitContainer1.Panel2.BackColor = SystemColors.Info;
+            splitContainer1.Panel2.BackColor = SystemColors.Window;
             splitContainer1.Panel2.Controls.Add(dataGridView1);
             splitContainer1.Panel2.Controls.Add(btnSave);
             splitContainer1.Panel2.Controls.Add(btnLoyalty);
@@ -107,21 +114,40 @@
             dataGridView1.AllowUserToResizeRows = false;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.BackgroundColor = SystemColors.Window;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = SystemColors.InactiveBorder;
+            dataGridView1.BorderStyle = BorderStyle.Fixed3D;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
             dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle1.ForeColor = SystemColors.MenuHighlight;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = Color.Gray;
+            dataGridViewCellStyle1.ForeColor = Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Window;
+            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
             dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
             dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Product, Quantity, Price });
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Product, Quantity, Price, IncreaseQty, DecreaseQty, Delete });
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = SystemColors.Window;
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle5.ForeColor = Color.Black;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Window;
+            dataGridViewCellStyle5.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
             dataGridView1.Dock = DockStyle.Fill;
             dataGridView1.GridColor = SystemColors.MenuText;
             dataGridView1.Location = new Point(0, 0);
+            dataGridView1.MultiSelect = false;
             dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = SystemColors.Window;
+            dataGridViewCellStyle6.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle6.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = SystemColors.Window;
+            dataGridViewCellStyle6.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
             dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             dataGridView1.ScrollBars = ScrollBars.Vertical;
             dataGridView1.Size = new Size(369, 499);
             dataGridView1.TabIndex = 4;
@@ -130,28 +156,66 @@
             // Product
             // 
             Product.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle2.BackColor = Color.DodgerBlue;
+            dataGridViewCellStyle2.BackColor = Color.White;
+            dataGridViewCellStyle2.ForeColor = Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = Color.White;
+            dataGridViewCellStyle2.SelectionForeColor = Color.Black;
             Product.DefaultCellStyle = dataGridViewCellStyle2;
             Product.HeaderText = "Product";
             Product.Name = "Product";
+            Product.ReadOnly = true;
             Product.Resizable = DataGridViewTriState.False;
             Product.SortMode = DataGridViewColumnSortMode.NotSortable;
             // 
             // Quantity
             // 
             Quantity.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle3.SelectionBackColor = Color.White;
+            dataGridViewCellStyle3.SelectionForeColor = Color.Black;
+            Quantity.DefaultCellStyle = dataGridViewCellStyle3;
             Quantity.HeaderText = "Quantity";
             Quantity.Name = "Quantity";
+            Quantity.ReadOnly = true;
             Quantity.Resizable = DataGridViewTriState.False;
             Quantity.SortMode = DataGridViewColumnSortMode.NotSortable;
             // 
             // Price
             // 
             Price.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle4.SelectionBackColor = Color.White;
+            dataGridViewCellStyle4.SelectionForeColor = Color.Black;
+            Price.DefaultCellStyle = dataGridViewCellStyle4;
             Price.HeaderText = "Price";
             Price.Name = "Price";
+            Price.ReadOnly = true;
             Price.Resizable = DataGridViewTriState.False;
             Price.SortMode = DataGridViewColumnSortMode.NotSortable;
+            // 
+            // IncreaseQty
+            // 
+            IncreaseQty.FillWeight = 50F;
+            IncreaseQty.HeaderText = "+";
+            IncreaseQty.Name = "IncreaseQty";
+            IncreaseQty.Resizable = DataGridViewTriState.False;
+            IncreaseQty.Text = "+";
+            IncreaseQty.UseColumnTextForButtonValue = true;
+            // 
+            // DecreaseQty
+            // 
+            DecreaseQty.FillWeight = 50F;
+            DecreaseQty.HeaderText = "-";
+            DecreaseQty.Name = "DecreaseQty";
+            DecreaseQty.Resizable = DataGridViewTriState.False;
+            DecreaseQty.Text = "-";
+            DecreaseQty.UseColumnTextForButtonValue = true;
+            // 
+            // Delete
+            // 
+            Delete.HeaderText = "Delete";
+            Delete.Name = "Delete";
+            Delete.Resizable = DataGridViewTriState.False;
+            Delete.Text = "Delete";
+            Delete.UseColumnTextForButtonValue = true;
             // 
             // OrderDetailsUserControl
             // 
@@ -179,5 +243,8 @@
         private DataGridViewTextBoxColumn Product;
         private DataGridViewTextBoxColumn Quantity;
         private DataGridViewTextBoxColumn Price;
+        private DataGridViewButtonColumn IncreaseQty;
+        private DataGridViewButtonColumn DecreaseQty;
+        private DataGridViewButtonColumn Delete;
     }
 }
