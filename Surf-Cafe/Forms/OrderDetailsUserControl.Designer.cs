f9060e8708076a71c26549cfab5ca2d96dbbb3d4
st10275468 <st10275468@vcconnect.edu.za>
2025-12-01 10:20:32 +0200

    Created OrderDetailsUserControl

---
+++ Surf-Cafe/Forms/OrderDetailsUserControl.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Surf_Cafe.Forms
+{
+    partial class OrderDetailsUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlContent = new Panel();
+            flpOrderSummary = new FlowLayoutPanel();
+            btnLoyalty = new Button();
+            btnPay = new Button();
+            btnSave = new Button();
+            SuspendLayout();
+            // 
+            // pnlContent
+            // 
+            pnlContent.Location = new Point(17, 109);
+            pnlContent.Name = "pnlContent";
+            pnlContent.Size = new Size(627, 421);
+            pnlContent.TabIndex = 0;
+            // 
+            // flpOrderSummary
+            // 
+            flpOrderSummary.Location = new Point(666, 109);
+            flpOrderSummary.Name = "flpOrderSummary";
+            flpOrderSummary.Size = new Size(280, 421);
+            flpOrderSummary.TabIndex = 1;
+            // 
+            // btnLoyalty
+            // 
+            btnLoyalty.Location = new Point(666, 553);
+            btnLoyalty.Name = "btnLoyalty";
+            btnLoyalty.Size = new Size(94, 23);
+            btnLoyalty.TabIndex = 0;
+            btnLoyalty.Text = "Loyalty";
+            btnLoyalty.UseVisualStyleBackColor = true;
+            // 
+            // btnPay
+            // 
+            btnPay.Location = new Point(766, 553);
+            btnPay.Name = "btnPay";
+            btnPay.Size = new Size(84, 23);
+            btnPay.TabIndex = 2;
+            btnPay.Text = "Payment";
+            btnPay.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(856, 553);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(90, 23);
+            btnSave.TabIndex = 3;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // OrderDetailsUserControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnSave);
+            Controls.Add(btnPay);
+            Controls.Add(btnLoyalty);
+            Controls.Add(flpOrderSummary);
+            Controls.Add(pnlContent);
+            Name = "OrderDetailsUserControl";
+            Size = new Size(1058, 616);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlContent;
+        private FlowLayoutPanel flpOrderSummary;
+        private Button btnLoyalty;
+        private Button btnPay;
+        private Button btnSave;
+    }
+}
