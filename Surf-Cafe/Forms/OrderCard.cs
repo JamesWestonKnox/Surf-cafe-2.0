c69e435838d18a49386b4a06bc84128b5fb4f8eb
James Knox <jamesknox361@gmail.com>
2025-12-01 10:22:32 +0200

    Added onclick event for order cards

--- Surf-Cafe/Forms/OrderCard.cs
+++ Surf-Cafe/Forms/OrderCard.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Surf_Cafe.Models;
+﻿using Surf_Cafe.Models;
 using System.Globalization;
 
 namespace Surf_Cafe.Forms
 {
     public partial class OrderCard : UserControl
     {
-        public Order Order {  get; set; }
+        public Order Order { get; set; }
+
+        public event Action<Order> OrderClicked;
         public OrderCard(Order order)
         {
             InitializeComponent();
             Order = order;
-
+            this.Click += OrderCard_Click;
             lblOrderName.Text = order.OrderName;
             lblDateCreated.Text = order.OrderDateTime.ToString("g");
             lblTotal.Text = $"Total: {order.OrderTotal.ToString("C", CultureInfo.CreateSpecificCulture("en-ZA"))}";
+        }
+
+        private void OrderCard_Click(object sender, EventArgs e)
+        {
+            OrderClicked?.Invoke(Order);
         }
 
         private void OrderCard_Load(object sender, EventArgs e)
