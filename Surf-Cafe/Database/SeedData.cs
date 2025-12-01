9946e8f9d8b0368238ad04e3fd0f0119f56f1381
James Knox <jamesknox361@gmail.com>
2025-12-01 13:31:15 +0200

    Moved user seeding to dedicated seeding class in database folder, called in program.cs

---
+++ Surf-Cafe/Database/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using Surf_Cafe.Models;
+
+namespace Surf_Cafe.Database
+{
+    public class SeedData
+    {
+        public void SeedUsers()
+        {
+            using (var db = new DBContext())
+            {
+                db.Database.EnsureCreated();
+
+                if (!db.Users.Any())
+                {
+                    var admin = new User
+                    {
+                        Username = "Admin",
+                        PasscodeHash = Authentication.HashPassword("admin25!"),
+                        Role = UserRole.Admin,
+                        DateRegistered = DateTime.Now
+                    };
+
+                    var emp = new User
+                    {
+                        Username = "Employee",
+                        PasscodeHash = Authentication.HashPassword("emp25!"),
+                        Role = UserRole.Staff,
+                        DateRegistered = DateTime.Now
+                    };
+
+                    db.Users.Add(admin);
+                    db.Users.Add(emp);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
